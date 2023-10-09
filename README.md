9eeec72e830f3428744093cc90d23f5cde243468
halecosar <halealtunakar@hotmail.com>
2023-10-09 15:05:50 +0300

    read me eklendi

---
+++ README.md
@@ -0,0 +1,4 @@
+Patika - Try-Catch Pratiği / Hafta5
+
+
+Soru: 10 elemanlı tek boyutlu bir dizi içerisinde kullanıcıdan alınan indeksteki elemanı döndüren bir Java metodu yazın. Eğer belirtilen indeks dizi boyutunun dışındaysa, metot bir hata mesajı döndürmelidir. Dizinin elmanlarını manuel olarak tanımlayın.
--- Try_Cath_odevi/src/Main.java
+++ Try_Cath_odevi/src/Main.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class Main extends Exception {
     static Scanner scan = new Scanner(System.in);
-    static int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    static int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //array elemanları tanımlandı.
     static boolean check = true;
 
     public static void main(String[] args) {
 
-        while (check == true) {
+        while (check == true) { //array içinde bir değer girilene kadar kullanıcıdan tekrar giriş yapması istendi.
 
             System.out.println(Arrays.toString(arr));
             System.out.println("Lütfen bir eleman seçiniz : ");
-            findArrElm();
+            findArrElm(); // metot çağırıldı.
 
 
         }
 
     }
 
-    public static void findArrElm() {
+    public static void findArrElm() { // metot oluşturuldu.
         try {
             int index = scan.nextInt();
             System.out.println("Dizideki " + index + ".eleman ==> " + arr[index]);
